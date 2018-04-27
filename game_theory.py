27a54751bf57cbc4901bd0f8b888eb985b0ba40a
Kyle Mistele <kyle@mistele.com>
2018-04-27 11:05:10 -0400

    Initial Commit
    Added To-do's

---
+++ game_theory.py
@@ -0,0 +1,7 @@
+"""
+#TODO: create interface for selecting AI's (terminal)
++ each AI should be a separate class
++ determine how to have AI's communicate with each other
++ need a score board/record-keeping system
++ store each class in a separate file
+"""
