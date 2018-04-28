fba1f851b89b5fb5f20fd6c4fff976ba3482727c
MisteleK <kyle@mistele.com>
2018-04-28 11:15:25 -0400

    Fixing a lot of class methods for the file writer, working a lot more with JSON. Created a JSON template for new games

--- file_writer.py
+++ file_writer.py
@@ -1,13 +1,25 @@
 import json as JSON
-
+from pprint import pprint
 # class for writing to/from files for communication between AI's
 class file_writer:
     def __init__(self, fname):
-        self.file = open("fname", "w+") # read and write
+        self.fname = f"{fname}.json"
+
+        # open template and storage file
+        with open("game_template.json", "r") as template:
+            self.template_data = JSON.load(template)
+        with open(self.fname, "r") as new_file:
+            self.file_data = JSON.load(new_file)
+
+        # if file is empty, create a new one based off of template
+        if "game_rounds" not in self.file_data:
+            with open(self.fname, "w") as new_file:
+                JSON.dump(self.template_data, new_file)
 
     def __repr__(self):
-        return self.file
-        #TODO: code to return all data in document so far
+        with open(self.fname, "r") as json_file:
+            pprint(JSON.load(json_file)) # print all data in file so far
+
 
     #method for encoding Python data to JSON
     def encode_json(self, to_encode):
@@ -22,17 +34,18 @@ class file_writer:
     #method for writing to file
     def write_to_file(self, to_write, want_encoded=False):
         if want_encoded == True:
-            self.file.write(self.encode_json(to_write))
-        else:
-            self.file.write(to_write)
+            with open(self.fname, "w+") as json_file:
+                JSON.dump(self.encode_json(to_write), json_file)
 
-    #method for getting last line of file
-    def read_last_line(self, want_decoded=False):
-        all_lines = self.file.readlines()
-        if want_decoded == True:
-            last_line = self.decode_json(all_lines[len(all_lines)-1])
-            return last_line
-        else:
-            last_line = all_lines[len(all_lines)-1]
-            return last_line
+    # getting all data in file
+    def read_file(self):
+        with open(self.fname, "r") as json_file:
+            data = JSON.load(json_file)
+            return data
+
+    # get results of last game
+    def get_last_game_results(self):
+        data = self.read_file["game_outcomes"]
+        last_game = data[len(data)-1]
+        return last_game
 
