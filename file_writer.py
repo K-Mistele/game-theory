01c441f4e26d5a2274db8b55da472576be70f74f
MisteleK <kyle@mistele.com>
2018-04-28 12:06:57 -0400

    Python + JSON file I/O so AI's will be able to communicate

--- file_writer.py
+++ file_writer.py
@@ -1,5 +1,6 @@
 import json as JSON
 from pprint import pprint
+import os.path
 # class for writing to/from files for communication between AI's
 class file_writer:
     def __init__(self, fname):
@@ -8,13 +9,16 @@ class file_writer:
         # open template and storage file
         with open("game_template.json", "r") as template:
             self.template_data = JSON.load(template)
-        with open(self.fname, "r") as new_file:
-            self.file_data = JSON.load(new_file)
 
-        # if file is empty, create a new one based off of template
-        if "game_rounds" not in self.file_data:
+        if os.path.isfile(self.fname):
+            with open(self.fname, "r+") as new_file:
+                self.file_data = JSON.load(new_file)
+        else:
             with open(self.fname, "w") as new_file:
                 JSON.dump(self.template_data, new_file)
+
+
+
 
     def __repr__(self):
         with open(self.fname, "r") as json_file:
@@ -22,30 +26,34 @@ class file_writer:
 
 
     #method for encoding Python data to JSON
-    def encode_json(self, to_encode):
-        json_string = JSON.dumps(to_encode)
-        return json_string
+    # def encode_json(self, to_encode):
+    #     json_string = JSON.dump(to_encode)
+    #     return json_string
 
     #method for decoding JSON to Python
-    def decode_json(self, to_decode):
-        python_data = JSON.loads(to_decode)
-        return python_data
+    # def decode_json(self, to_decode):
+    #     python_data = JSON.load(to_decode)
+    #     return python_data
 
     #method for writing to file
-    def write_to_file(self, to_write, want_encoded=False):
+    def write_to_file(self, to_write, want_encoded=True):
         if want_encoded == True:
             with open(self.fname, "w+") as json_file:
-                JSON.dump(self.encode_json(to_write), json_file)
+                JSON.dump(to_write, json_file)
+        else:
+            with open(self.fname, "w+") as json_file:
+                JSON.dump(to_write, json_file)
 
     # getting all data in file
-    def read_file(self):
+    def get_data(self):
         with open(self.fname, "r") as json_file:
             data = JSON.load(json_file)
             return data
 
     # get results of last game
     def get_last_game_results(self):
-        data = self.read_file["game_outcomes"]
+
+        data = self.get_data()["game_outcomes"]
         last_game = data[len(data)-1]
         return last_game
 
--- game_theory.py
+++ game_theory.py
@@ -6,4 +6,34 @@
 + store each class in a separate file
 """
 import json
+from file_writer import file_writer
+from random import randint
+from pprint import pprint
 # For guide to Python with JSON, see http://docs.python-guide.org/en/latest/scenarios/json/
+name = "random_v_random" #TODO: will later be determined by which AI's are engaged, hard-coded for now
+json_manager = file_writer(name)
+
+#"Game" Loop
+loops = int(input("How many rounds would you like to start?"))
+
+for i in range(0, loops):
+    # generate random booleans
+    rand1 = randint(1,4)
+    rand2 = randint(1,4)
+    if rand1 % 2 == 0:
+        val1 = True
+    else:
+        val1 = False
+
+    if rand2 % 2 == 0:
+        val2 = True
+    else:
+        val2 = False
+
+    game_results = [val1, val2]
+    data = json_manager.get_data()
+    data["game_rounds"] += 1
+    data["game_outcomes"].append(game_results)
+
+    json_manager.write_to_file(data)
+pprint(json_manager.get_data())
