457c825fbedd3aa1a1cffb1f578d0d2844772483
MisteleK <kyle@mistele.com>
2018-05-13 15:10:45 -0400

    decision mathod based on current state

--- classes/decision_matrix.py
+++ classes/decision_matrix.py
@@ -1,5 +1,5 @@
 class decision_matrix:
-    
+
     def __init__(self, probabilities=((1,0),(1,0))):
         self.matrix = {
             "cooperate": {
@@ -11,5 +11,14 @@ class decision_matrix:
                 "compete": probabilities[1][1]
             }
         }
+    def get_decision(self, state):
+        if state == "cooperate" or state == "compete":
+            for key in self.matrix[state]:
+                #iterate through keys and whichever one has a probability of 1 is returned
+                if self.matrix[state][key] == 1:
+                    return key
+        else:
+            #error state
+            return "cooperate"
 
 
