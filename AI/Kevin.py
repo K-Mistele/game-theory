bb0e24338c386459f6b84cecccda707b3a5b8549
K-Mistele <kyle@mistele.com>
2018-08-23 12:12:28 -0400

    create Kevin AI (tit-for-two-tats)

---
+++ AI/Kevin.py
@@ -0,0 +1,18 @@
+class Kevin:
+
+    behavior_string = "Tit-for-two-tats AI"
+    gender = "male"
+    name = "Kevin"
+
+    def first_turn(self):
+        return "cooperate"
+
+    def take_turn(self, data):
+
+        #create list of opponent's last two moves
+        last_two_opponent_moves = [data[-1][1], data[-2][1]]d
+        #if opponent has defected twice, defect, otherwise cooperate
+        if "cooperate" not in last_two_opponent_moves:
+            return "compete"
+        else:
+            return "cooperate"
